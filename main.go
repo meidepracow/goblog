be67693db1fba53dfcf9662965b603a0965a5d83
wumaoquan <597991840@qq.com>
2022-04-19 18:58:44 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -34,7 +34,17 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+
+	err := r.ParseForm()
+	if err != nil {
+		//解析错误
+		fmt.Fprint(w, "请填写正确的数据！")
+		return
+	}
+	title := r.FormValue("title")
+	content := r.FormValue("body")
+	fmt.Fprintf(w, "title 的值为: %v <br>", title)
+	fmt.Fprintf(w, "content 的值为: %v", content)
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -55,7 +65,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 		<title>创建文章 —— 我的技术博客</title>
 	</head>
 	<body>
-		<form action="%s" method="post">
+		<form action="%s?test=data" method="post">
 			<p><input type="text" name="title"></p>
 			<p><textarea name="body" cols="30" rows="10"></textarea></p>
 			<p><button type="submit">提交</button></p>
